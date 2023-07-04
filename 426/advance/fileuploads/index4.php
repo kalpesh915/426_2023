270309c59deee1777746e78f336a69f11b80e786
Kalpesh Chauhan <63454815+kalpesh915@users.noreply.github.com>
2023-07-04 17:51:41 +0530

    4-7

---
+++ 426/advance/fileuploads/index4.php
@@ -0,0 +1,51 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post" enctype="multipart/form-data">
+        <input type="file" name="file1" required accept=".jpg">
+        <input type="submit" value="Upload Now" name="uploadProcess">
+        <hr>
+    </form>
+    <?php
+        if(isset($_SESSION["msg"])){
+            echo "<hr>".$_SESSION["msg"]."<hr>";
+            unset($_SESSION["msg"]);
+        }
+    ?>
+</body>
+</html>
+
+
+<?php
+    if(isset($_POST["uploadProcess"])){
+        $file1 = $_FILES["file1"];
+        print_r($file1);
+
+        $source = $file1["tmp_name"];
+        $destination = "files/".$file1["name"];
+        $type = $file1["type"];
+
+        if($type === "image/jpeg" || $type === "image/jpg"){
+            if(!file_exists($destination)){
+                move_uploaded_file($source, $destination);
+                $_SESSION["msg"] = "File Uploaded Successfully";
+            }else{
+                $_SESSION["msg"] = "File Already Exist in Folder";
+            }
+            
+        }else{
+            $_SESSION["msg"] = "Must Select *.JPG files Only.";
+        }
+
+        header("location:index4.php");
+    }
+?>
---
+++ 426/advance/fileuploads/index5.php
@@ -0,0 +1,55 @@
+<?php
+    session_start();
+    date_default_timezone_set("Asia/Kolkata");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post" enctype="multipart/form-data">
+        <input type="file" name="file1" required accept=".jpg">
+        <input type="submit" value="Upload Now" name="uploadProcess">
+        <hr>
+    </form>
+    <?php
+        if(isset($_SESSION["msg"])){
+            echo "<hr>".$_SESSION["msg"]."<hr>";
+            unset($_SESSION["msg"]);
+        }
+    ?>
+</body>
+</html>
+
+
+<?php
+    if(isset($_POST["uploadProcess"])){
+        $file1 = $_FILES["file1"];
+        print_r($file1);
+
+        $source = $file1["tmp_name"];
+        //$name = date("dmY_his_a")."-".$file1["name"];
+        //$name = time()."-".$file1["name"];
+        $name = date("dmY_his_a")."_".rand(9999, 99999)."_".$file1["name"];
+        $destination = "files/".$name;
+        $type = $file1["type"];
+
+        if($type === "image/jpeg" || $type === "image/jpg"){
+            if(!file_exists($destination)){
+                move_uploaded_file($source, $destination);
+                $_SESSION["msg"] = "File Uploaded Successfully";
+            }else{
+                $_SESSION["msg"] = "File Already Exist in Folder";
+            }
+            
+        }else{
+            $_SESSION["msg"] = "Must Select *.JPG files Only.";
+        }
+
+        header("location:index5.php");
+    }
+?>
